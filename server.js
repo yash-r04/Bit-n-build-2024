c7c8aa0145d57828e5c5bb4d41fb688862f52570
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2024-09-28 16:56:14 +0530

    data backend partially added

--- server.js
+++ server.js
@@ -36,9 +36,14 @@ app.get('/dashboard', (req, res) => {
     if (!req.isAuthenticated()) {
         return res.redirect('/');
     }
-    res.sendFile(__dirname + '/views/tasks.html');
+    res.sendFile(__dirname + '/views/dashboard.html');
 });
 
 app.listen(8080, () => {
     console.log('Server running on http://localhost:8080');
+});
+app.get('/user-info', async (req, res) => {
+    // Fetch the user from the database based on some identifier like req.userId
+    const user = await User.findById(req.userId);  // Assume you have the user's ID in req.userId
+    res.json({ name: user.name });
 });
